0a53db2a9ba63c7a0c30ef48e6cf6b25ef3bfac9
anotherFeng <regnagleppod@gmail.com>
2018-12-20 23:56:31 -0500

    Setup for scss config

--- react-bnb/src/App.js
+++ react-bnb/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from './shared/Header';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
