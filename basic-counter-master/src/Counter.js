5529bc4fcb6e3c9cd88bb384971cdfd6f5bfa7de
Feng Desktop <regnagleppod@gmail.com>
2019-03-04 22:52:04 -0500

    Add counter implementation using default component state

---
+++ basic-counter-master/src/Counter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  handleReset = () => {
+    this.setState({ count: 0 });
+  }
+
+  render() {
+    return (
+      <section className="Counter">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleIncrement} className="full-width">Increment</button>
+        <button onClick={this.handleDecrement} className="full-width">Decrement</button>
+        <button onClick={this.handleReset} className="full-width">Reset</button>
+      </section>
+    );
+  }
+}
