f2bf34b16aa8475c4ad656ef0b9d836f29b983a6
anotherFeng <regnagleppod@gmail.com>
2019-03-07 02:56:36 -0500

    Add container design and add pizza-calculator

--- basic-counter/src/Counter.js
+++ basic-counter/src/Counter.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  }
-
-  handleReset = () => {
-    this.setState({ count: 0 });
-  }
-
   render() {
+    const { count, onIncrement, onDecrement, onReset } = this.props;
     return (
       <section className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleIncrement} className="full-width">Increment</button>
-        <button onClick={this.handleDecrement} className="full-width">Decrement</button>
-        <button onClick={this.handleReset} className="full-width">Reset</button>
+        <h1>Count: { count }</h1>
+        <button onClick={onIncrement} className="full-width">Increment</button>
+        <button onClick={onDecrement} className="full-width">Decrement</button>
+        <button onClick={onReset} className="full-width">Reset</button>
       </section>
     );
   }
--- basic-counter/src/CounterContainer.js
+++ basic-counter/src/CounterContainer.js
@@ -1,1 +1,31 @@
-// Intentionally left blank.
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+export default class CounterContainer extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  handleReset = () => {
+    this.setState({ count: 0 });
+  }
+
+  render() {
+    return (
+      <Counter 
+        count={this.state.count}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onReset={this.handleReset}
+        />
+    );
+  }
+}
